4f448e65f406f213443b2d0090b027634243792a
Induwara Uthsara <induwarauthsara19@gmail.com>
2021-09-16 17:20:15 +0530

    Spinnner Completed

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 
 import Tabs from "../components/Tabs";
+import Spinner from "../components/spinner";
 
 const Dashboard = () => {
   const contents = [
-    { title: "Books", elements: <h1>Book Content</h1> },
+    { title: "Books", elements: <Spinner /> },
     { title: "Members", elements: <h1>Members Content</h1> },
   ];
   return <Tabs contents={contents} />;
