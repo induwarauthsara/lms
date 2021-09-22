feef7ee170156ba4be1518adba66059e3139d40b
Induwara Uthsara <induwarauthsara19@gmail.com>
2021-09-22 22:22:38 +0530

    implement table

--- src/components/Table.js
+++ src/components/Table.js
@@ -12,40 +12,46 @@ const StyledTable = styled.table`
   }
   tbody tr {
     :nth-of-type(odd) {
-      background-color: #efefef;
+      background-color: #ddd;
     }
     :hover {
-      background-color: lightpink;
+      background-color: ${(props) => props.theme.primary.light};
     }
   }
   thead > tr {
-    background-color: #c2c2c2;
+    background-color: ${(props) => props.theme.primary.main};
+    color: white;
+    text-transform: capitalize;
   }
 `;
 
 const TabelMarkup = ({ titles, data }) => (
   <StyledTable>
     <colgroup>
+      {" "}
       {titles.map((title, index) => (
         <col key={index} />
-      ))}
-    </colgroup>
+      ))}{" "}
+    </colgroup>{" "}
     <thead>
       <tr>
+        {" "}
         {titles.map((title, index) => (
-          <th key={index}>{title}</th>
-        ))}
-      </tr>
-    </thead>
+          <th key={index}> {title} </th>
+        ))}{" "}
+      </tr>{" "}
+    </thead>{" "}
     <tbody>
+      {" "}
       {data.map((item, index) => (
         <tr key={index}>
+          {" "}
           {titles.map((title, index) => (
-            <td key={index}>{item[title]}</td>
-          ))}
+            <td key={index}> {item[title]} </td>
+          ))}{" "}
         </tr>
-      ))}
-    </tbody>
+      ))}{" "}
+    </tbody>{" "}
   </StyledTable>
 );
 
--- src/containers/Dashboard/Books.jsx
+++ src/containers/Dashboard/Books.jsx
@@ -4,10 +4,21 @@ import Table from "../../components/Table";
 import { FluidContainer } from "../../components/CommonComponents";
 
 const Books = ({ catalog }) => {
+  const updatedCatalog = [
+    ...catalog,
+    {
+      author: "Induwara Uthsara",
+      burrowedDate: "",
+      burrowedMemberId: "",
+      id: "3",
+      isAvailable: "true",
+      title: "Ethical Hacking Handbook",
+    },
+  ];
   return (
     <FluidContainer>
       {/* {catalog[0] && catalog[0].title ? catalog[0].title : "Still Rendering"} */}
-      <Table data={catalog} />
+      <Table data={updatedCatalog} />
     </FluidContainer>
   );
 };
