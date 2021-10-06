0a2037076395644be5f5f7425569932c53c8404b
Induwara Uthsara <induwarauthsara19@gmail.com>
2021-10-06 16:08:57 +0530

    Implement MemberAPI Demo

---
+++ src/api/memberAPI.js
@@ -0,0 +1,7 @@
+export const getMembers = () => [
+  { id: "1", name: "Induwara Uhsara" },
+  { id: "2", name: "Red Bird" },
+  { id: "3", name: "Walter O'Brien" },
+  { id: "4", name: "Bruce Wayne" },
+  { id: "5", name: "Peter Parker" },
+];
